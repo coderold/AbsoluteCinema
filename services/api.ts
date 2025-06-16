0b70281b43b1e15f7a90fcbd0a3ab73c6cbffc18
coderold <mm35molina@gmail.com>
2025-06-16 19:15:50 +0800

    search function - movie api set up

---
+++ services/api.ts
@@ -0,0 +1,13 @@
+// const url = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
+// const options = {
+//   method: 'GET',
+//   headers: {
+//     accept: 'application/json',
+//     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkMGIzZWRlYzdkODhjMTllY2ZiNDBmNzU1YzhjYWJlNSIsIm5iZiI6MTc1MDA3MTM0OS4yMzIsInN1YiI6IjY4NGZmODM1ZDg3YTE1MWM1MzM0NDk0NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.3ckhAlOmWpbZ8kgoMxMreB1Jyqdjq4wmjH3Ntsxv_Fg'
+//   }
+// };
+
+// fetch(url, options)
+//   .then(res => res.json())
+//   .then(json => console.log(json))
+//   .catch(err => console.error(err));
